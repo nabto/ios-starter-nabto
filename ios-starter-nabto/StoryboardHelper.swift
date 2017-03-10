65af775811ffd27adb8ca8e7ac0fcb8947b94caa
Ulrik Gammelby <ug@nabto.com>
2017-03-10 17:00:30 +0000

    initial commit based on Tiago Lira's work

---
+++ ios-starter-nabto/StoryboardHelper.swift
@@ -0,0 +1,32 @@
+//
+//  StoryboardHelper.swift
+//  ios-starter-nabto
+//
+//  Created by Tiago Lira on 03/02/2017.
+//  Copyright © 2017 Nabto. All rights reserved.
+//
+
+import UIKit
+
+// To add custom view controllers for your devices:
+// 1 - Create a subclass of DeviceViewController
+// 2 - Add it to the storyboard with correct storyboardID
+// 3 - Add it to the options in this method
+
+class StoryboardHelper {
+    
+    class func viewControllerFor(device: NabtoDevice) -> DeviceViewController? {
+        
+        let storyboard =  UIStoryboard(name: "Main", bundle: nil)
+        
+        var controller : DeviceViewController?
+        
+        //Add custom device screens here
+        if device.product == "ACME 9002 Heatpump" {
+            controller = storyboard.instantiateViewController(withIdentifier: "ACMEHeaterViewController") as! ACMEHeaterViewController
+        }
+        
+        controller?.device = device
+        return controller
+    }
+}
---
+++ ios-starter-nabto/ios-starter-nabto-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ios-starter-nabto-Bridging-Header.h
+//  ios-starter-nabto
+//
+//  Created by Tiago Lira on 30/01/2017.
+//  Copyright © 2017 Nabto. All rights reserved.
+//
+
+#ifndef ios_starter_nabto_Bridging_Header_h
+#define ios_starter_nabto_Bridging_Header_h
+
+#endif /* ios_starter_nabto_Bridging_Header_h */
+
+//#import "nabto_client_api.h"
+#import "Manager.h"
